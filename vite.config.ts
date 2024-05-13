4e30bd015b38e05534adae50f379ba4e3b2a71df
Anatolii Vasilev <vas11yev.work@ya.ru>
2024-05-13 16:06:37 +0500

    feat: add pwa options

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,34 @@
 import path from 'path';
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: ['icon.svg', 'icon_192.png', 'icon_512.png'],
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Multimoney',
+        short_name: 'Multimoney',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icon_192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icon_512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.join(__dirname, './src'),
