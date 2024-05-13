cc5c5dd7554e7486f98a78a95ef21f12e6696d84
Anatolii Vasilev <vas11yev.work@ya.ru>
2024-05-13 16:15:57 +0500

    fix: save area

--- src/widgets/user-footer/ui/UserFooter.vue
+++ src/widgets/user-footer/ui/UserFooter.vue
@@ -1,6 +1,6 @@
 <template>
-  <div :class="$style.footer" class="fixed bottom-0 grid h-16 w-full grid-cols-4 rounded-t-xl bg-white">
-    <div v-for="item in items" :key="item.label" class="flex flex-col items-center justify-center">
+  <div :class="$style.footer" class="fixed bottom-0 grid w-full grid-cols-4 rounded-t-xl bg-white">
+    <div v-for="item in items" :key="item.label" class="flex h-16 flex-col items-center justify-center">
       <RouterLink :to="{ name: item.route }" class="flex flex-col text-gray-400" active-class="!text-blue-500">
         <UiIcon :icon-name="item.icon" />
         <UiTypo level="6" class="mt-1">{{ item.label }}</UiTypo>
