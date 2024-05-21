34ff81f7f9dc6769a277988db80f34e07de703aa
Anatolii Vasilev <vas11yev.work@ya.ru>
2024-05-21 18:54:43 +0500

    feat: add debug page

---
+++ src/pages/debug/ui/DebugPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="p-3">
+    <UiTypo type="heading" class="mb-3 font-semibold">Debug page</UiTypo>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { UiTypo } from '@/shared/ui';
+</script>
